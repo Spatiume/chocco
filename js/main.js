ab4bc0b45c61fdcc91842450a46114a56a1078e5
Pavel Sysoev <sysoevpvl@gmail.com>
2024-09-19 02:33:07 +0300

    accardeon team completed

--- js/main.js
+++ js/main.js
@@ -82,3 +82,20 @@ slider.addEventListener('click', function (event) {
   }
 
 })
+
+
+const teamlist = document.querySelector('.team__list');
+
+teamlist.addEventListener('click', function (event) {
+
+  if (!event.target.closest('.teammate')) return;
+
+  if (event.target.closest('.teammate').classList.contains('teammate--active')) {
+    event.target.closest('.teammate').classList.remove('teammate--active');
+    return;
+  }
+  if(teamlist.querySelector('.teammate--active')){
+    teamlist.querySelector('.teammate--active').classList.remove('teammate--active');
+  }
+  event.target.closest('.teammate').classList.add('teammate--active');
+})
