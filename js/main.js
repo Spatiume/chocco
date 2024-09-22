f53a9a07258f4f43a092ac7dfbb2c4f3b9b5e171
Pavel Sysoev <sysoevpvl@gmail.com>
2024-09-22 19:34:24 +0300

    reviews section js  completed

--- js/main.js
+++ js/main.js
@@ -110,25 +110,43 @@ choccoMenu.addEventListener('click', function (event) {
 })
 
 
-const review = document.querySelector('.review');
+//reviews section 
 
+const reviewDom = document.querySelector('.review');
+const reviewsDom = document.querySelector('.reviews');
 const formReview = document.querySelector('.form-review');
+let overlay;
+let reviewJson = [{
+  id: 0,
+  title: 'Лучший перекус',
+  text: 'Я ем малыми порциями, но часто. Это повышает мою продуктивность в разы. В течение дня сижу за компьютером и часто отходить в кафе или на кухню просто не могу себе позволить. А батончик – он всегда под рукой. Это приятно и удобно.',
+  author: 'Алёна Хмельницкая',
+  img: './img/reviews/review1.jpg',
+},
+{
+  id: 1,
+  title: 'Супер, я люблю покушать',
+  text: 'Я ем малыми порциями, но часто. Это повышает мою продуктивность в разы. В течение дня сижу за компьютером и часто отходить в кафе или на кухню просто не могу себе позволить. А батончик – он всегда под рукой. Это приятно и удобно.',
+  author: 'Виктор Устрица',
+  img: './img/reviews/review2.jpg',
+},
+{
+  id: 2,
+  title: 'Кайф, очень понравилось',
+  text: 'Я ем малыми порциями, но часто. Это повышает мою продуктивность в разы. В течение дня сижу за компьютером и часто отходить в кафе или на кухню просто не могу себе позволить. А батончик – он всегда под рукой. Это приятно и удобно.',
+  author: 'Евгения Гагарина',
+  img: './img/reviews/review3.jpg',
+},];
+
 
 formReview.addEventListener('submit', function (event) {
   event.preventDefault();
-
-  function createNewReview() {
-    let newReview = { title: '', text: '', author: '', img: '' };
-    newReview.title = formReview.querySelector('#form-review__title').value;
-    newReview.author = formReview.querySelector('#form-review__author').value;
-    newReview.text = formReview.querySelector('#form-review__text').value;
-    newReview.img = formReview.querySelector('img').src;
-    if (newReview.img < 100) newReview.img.src = './../img/reviews/camera.png';
-    return newReview;
-  }
-
-  console.log(createNewReview());
-
+  let review = createNewReview();
+  reviewJson.push(review)
+  createNewReviewDom(review);
+  showReview(review);
+  overlay.close();
+  formReview.reset();
 });
 
 formReview.querySelector('input[type=file]').addEventListener('change', function (event) {
@@ -137,7 +155,7 @@ formReview.querySelector('input[type=file]').addEventListener('change', function
   function getReviewImgSrc(url) {
     formReview.querySelector('img').src = url;
   }
-  
+
   function toBase64(file, getReviewImgSrc) {
     let reader = new FileReader();
     reader.readAsDataURL(file);
@@ -149,55 +167,79 @@ formReview.querySelector('input[type=file]').addEventListener('change', function
 
 
 
+reviewsDom.addEventListener('click', function (event) {
+  event.preventDefault();
+  let item = event.target.closest('[data-review-id]');
+  if (!item) return;
+  let itemId = item.dataset.reviewId
+  item.closest('.reviews__list').querySelector('.reviews__item--active').classList.remove('reviews__item--active');
+  item.classList.add('reviews__item--active')
+  showReview(reviewJson[itemId]);
+});
 
 
-
-
-
-
-
-
-
-let newReview = [{
-  title: 'Лучший перекус',
-  text: 'Я ем малыми порциями, но часто. Это повышает мою продуктивность в разы. В течение дня сижу за компьютером и часто отходить в кафе или на кухню просто не могу себе позволить. А батончик – он всегда под рукой. Это приятно и удобно.',
-  author: 'Алёна Хмельницкая',
-  img: './img/reviews/review1.jpg',
-},
-{
-  title: 'Супер, я люблю покушать',
-  text: 'Я ем малыми порциями, но часто. Это повышает мою продуктивность в разы. В течение дня сижу за компьютером и часто отходить в кафе или на кухню просто не могу себе позволить. А батончик – он всегда под рукой. Это приятно и удобно.',
-  author: 'Виктор Устрица',
-  img: './img/reviews/review2.jpg',
-},
-{
-  title: 'Кайф, очень понравилось',
-  text: 'Я ем малыми порциями, но часто. Это повышает мою продуктивность в разы. В течение дня сижу за компьютером и часто отходить в кафе или на кухню просто не могу себе позволить. А батончик – он всегда под рукой. Это приятно и удобно.',
-  author: 'Евгения Гагарина',
-  img: './img/reviews/review3.jpg',
-},
-
-
-]
-
-
-review.addEventListener('click', function (event) {
-  console.log('click');
-  createReview(review, newReview[2]);
-})
-
-function createReview(review, newReview) {
-  let title = review.querySelector('.review__title');
-  let text = review.querySelector('.review__text');
-  let author = review.querySelector('.review__author');
-  let img = review.querySelector('.review__img');
-
-  title.textContent = newReview.title;
-  text.innerHTML = `<p>${newReview.text}</p>`;
-  author.textContent = newReview.author;
-  console.log(img);
-  img.src = './../img/reviews/review3.jpg';
-
+function createNewReview() {
+  let newReview = { id: '', title: '', text: '', author: '', img: '', };
+  newReview.title = formReview.querySelector('#form-review__title').value;
+  newReview.author = formReview.querySelector('#form-review__author').value;
+  newReview.text = formReview.querySelector('#form-review__text').value;
+  newReview.img = formReview.querySelector('img').src;
+  newReview.id = reviewJson.length;
+  if (newReview.img < 100) newReview.img.src = './../img/reviews/camera.png';
+  return newReview;
 }
 
+function createNewReviewDom(review) {
+  let newReviewDom = reviewsDom.querySelector('.reviews__item').cloneNode(true);
+  reviewsDom.querySelector('.reviews__list').lastElementChild.before(newReviewDom);
+  newReviewDom.dataset.reviewId = review.id;
+  newReviewDom.querySelector('img').src = review.img;
+  newReviewDom.click();
+}
 
+function showReview(review) {
+  let title = reviewDom.querySelector('.review__title');
+  let text = reviewDom.querySelector('.review__text');
+  let author = reviewDom.querySelector('.review__author');
+  let img = reviewDom.querySelector('.review__img');
+
+  title.textContent = review.title;
+  text.innerHTML = `<p>${review.text}</p>`;
+  author.textContent = review.author;
+  img.src = review.img;
+}
+
+const reviewBtnNewReview = document.querySelector('.reviews-btn__new');
+
+reviewBtnNewReview.addEventListener('click', function (event) {
+  event.preventDefault();
+  const template = event.target.closest('section').querySelector('.overlay')
+  if (!template) return;
+  overlay = createOverlay(template);
+  overlay.open();
+});
+
+function createOverlay(template) {
+  const overlay = template;
+  const closeBtn = template.querySelector('.closebtn');
+
+  overlay.addEventListener('click', (event) => {
+    if (event.target == overlay) closeBtn.click();
+  })
+
+  closeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    document.body.style.overflow = '';
+    overlay.style.display = 'none';
+  })
+
+  return {
+    open() {
+      overlay.style.display = 'flex';
+      document.body.style.overflow = 'hidden';
+    },
+    close() {
+      closeBtn.click();
+    },
+  }
+}
