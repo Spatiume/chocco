ec0298e254e633480eb8d495829fa89f00badce7
Pavel Sysoev <sysoevpvl@gmail.com>
2024-09-24 02:49:22 +0300

    player Youtube API completed

--- js/main.js
+++ js/main.js
@@ -282,28 +282,6 @@ const init = () => {
 
   myMap.geoObjects.add(myCollection);
 
-  // let myPolyline = new ymaps.GeoObject({
-  //   geometry: {
-  //     type: "LineString",
-  //     coordinates: [
-  //       [59.891723, 30.318062],
-  //       [59.891286, 30.318389],
-  //       [59.891033, 30.318968],
-  //       [59.890166, 30.319011],
-  //       [59.888799, 30.319169],
-  //       [59.888810, 30.319963],
-  //       [59.888826, 30.323838],
-  //       [59.888875, 30.325052],
-  //       [59.888949, 30.330195],
-  //       [59.8890801, 30.330195],
-  //     ],
-  //     strokeWidth: 6,
-  //     strokeColor: 'red',
-  //     draggable: true
-  //   }
-  // });
-
-
   var myPolyline = new ymaps.Polyline([
     [59.891723, 30.318062],
     [59.891286, 30.318389],
@@ -311,19 +289,18 @@ const init = () => {
     [59.890166, 30.319011],
     [59.888799, 30.319169],
     [59.888810, 30.319963],
+    [59.888824, 30.323808],
     [59.888826, 30.323838],
+    [59.888834, 30.324264],
     [59.888875, 30.325052],
     [59.888949, 30.330295],
     [59.8890801, 30.330295],
   ], {},
     {
-      strokeWidth: 5,
+      strokeWidth: 4,
       strokeColor: '#365a49',
       draggable: false
     });
-
-
-  console.log(myPolyline);
 
   myMap.geoObjects.add(myPolyline);
   myPolyline.editor.startEditing();
@@ -342,4 +319,143 @@ mapInfo.addEventListener('click', function (event) {
   }
 });
 
+let player;
+const playerContainer = document.querySelector('.player')
 
+let eventsInit = () => {
+
+  document.querySelector('.player__start').addEventListener('click', (e) => {
+    e.preventDefault();
+    player.playVideo();
+  });
+
+  document.querySelector('.player__pause').addEventListener('click', (e) => {
+    e.preventDefault();
+    player.pauseVideo();
+  });
+
+  document.querySelector('.player__splash-start').addEventListener('click', (e) => {
+    e.preventDefault();
+    player.playVideo();
+  });
+
+  document.querySelector('.player__playback').addEventListener('click', (e) => {
+    const bar = e.target;
+    const clickedPosition = e.clientX - bar.getBoundingClientRect().left;
+    const newButtonPositionPercent = (clickedPosition / bar.getBoundingClientRect().width) * 100;
+    const newPlaybackPositionSec = (player.getDuration() / 100) * newButtonPositionPercent;
+
+    document.querySelector('.player__playback-button').style.left = newPlaybackPositionSec + '%';
+
+    player.seekTo(newPlaybackPositionSec);
+
+  })
+
+  document.querySelector('.volume__button').addEventListener('click', (e) => {
+    if (player.isMuted()) {
+      playerContainer.classList.remove('player--mute');
+      document.querySelector('.volume__level-button').style.left = player.getVolume() + '%';
+      player.unMute()
+    } else {
+      playerContainer.classList.add('player--mute');
+      document.querySelector('.volume__level-button').style.left = 0 + '%';
+      player.mute()
+    }
+  });
+
+  document.querySelector('.volume__level').addEventListener('click', (e) => {
+    const bar = e.target;
+    const clickedPosition = e.clientX - bar.getBoundingClientRect().left;
+    const newButtonPositionPercent = (clickedPosition / bar.getBoundingClientRect().width) * 100;
+
+
+    if (playerContainer.classList.contains('player--mute') && newButtonPositionPercent > 0) {
+      playerContainer.classList.remove('player--mute');
+    }
+
+    if (newButtonPositionPercent == 0) {
+      playerContainer.classList.add('player--mute');
+    }
+
+    document.querySelector('.volume__level-button').style.left = newButtonPositionPercent + '%';
+
+    player.setVolume(newButtonPositionPercent);
+
+  });
+}
+
+function formatTime(timeSec) {
+  const roundTime = Math.round(timeSec);
+
+  const minutes = addZero(Math.floor(roundTime / 60));
+  const seconds = addZero(Math.floor(roundTime - minutes * 60));
+
+  function addZero(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  return `${minutes}:${seconds}`
+}
+
+const onPlayerReady = () => {
+  let interval;
+
+  const durationSec = player.getDuration();
+  document.querySelector('.player__duration-estimate').textContent = formatTime(durationSec);
+
+  if (interval !== undefined) {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    const completedSec = player.getCurrentTime();
+    document.querySelector('.player__duration-completed').textContent = formatTime(completedSec);
+
+    const completedPercent = (completedSec / durationSec * 100);
+    document.querySelector('.player__playback-button').style.left = completedPercent + '%'
+  }, 1000)
+
+  const volumeLevel = player.getVolume();
+  document.querySelector('.volume__level-button').style.left = volumeLevel + '%';
+}
+
+const onPlayerStateChange = event => {
+  /*
+    -1 (воспроизведение видео не начато), 0 (воспроизведение видео завершено)
+    1 (воспроизведение), 2 (пауза), 3 (буферизация), 5 (видео подают реплики).
+  */
+  switch (event.data) {
+    case 1:
+      playerContainer.classList.add('player--active')
+      playerContainer.classList.remove('player--paused');
+      break;
+    case 2:
+      playerContainer.classList.remove('player--active')
+      playerContainer.classList.add('player--paused');
+      break;
+  }
+};
+
+
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("yt-player", {
+    height: "405",
+    width: "660",
+    videoId: "DISjdfkSjc8",
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    },
+    playerVars: {
+      controls: 0,
+      disablekb: 1,
+      showinfo: 0,
+      rel: 0,
+      autoplay: 0,
+      modestbranding: 0
+    }
+  });
+}
+
+eventsInit();
