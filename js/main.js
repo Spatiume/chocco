12016549b89b278ffb0e99880fdb8877ab2175ba
Pavel Sysoev <sysoevpvl@gmail.com>
2024-09-23 19:06:45 +0300

    yandex map API completed

--- js/main.js
+++ js/main.js
@@ -243,3 +243,103 @@ function createOverlay(template) {
     },
   }
 }
+
+
+
+
+
+
+
+
+
+
+// YANDEX map
+
+
+
+let myMap;
+const init = () => {
+  myMap = new ymaps.Map("map", {
+    center: [59.889140, 30.325289],
+    zoom: 15,
+    controls: [],
+  });
+
+  let coords = [
+    [59.889240, 30.330789],
+  ],
+    myCollection = new ymaps.GeoObjectCollection({}, {
+      draggable: false,
+      iconLayout: 'default#image',
+      iconImageHref: './icons/marker.svg',
+      iconImageSize: [46, 57],
+      iconImageOffset: [-35, -52]
+    });
+
+  for (let i = 0; i < coords.length; i++) {
+    myCollection.add(new ymaps.Placemark(coords[i]));
+  }
+
+  myMap.geoObjects.add(myCollection);
+
+  // let myPolyline = new ymaps.GeoObject({
+  //   geometry: {
+  //     type: "LineString",
+  //     coordinates: [
+  //       [59.891723, 30.318062],
+  //       [59.891286, 30.318389],
+  //       [59.891033, 30.318968],
+  //       [59.890166, 30.319011],
+  //       [59.888799, 30.319169],
+  //       [59.888810, 30.319963],
+  //       [59.888826, 30.323838],
+  //       [59.888875, 30.325052],
+  //       [59.888949, 30.330195],
+  //       [59.8890801, 30.330195],
+  //     ],
+  //     strokeWidth: 6,
+  //     strokeColor: 'red',
+  //     draggable: true
+  //   }
+  // });
+
+
+  var myPolyline = new ymaps.Polyline([
+    [59.891723, 30.318062],
+    [59.891286, 30.318389],
+    [59.891033, 30.318968],
+    [59.890166, 30.319011],
+    [59.888799, 30.319169],
+    [59.888810, 30.319963],
+    [59.888826, 30.323838],
+    [59.888875, 30.325052],
+    [59.888949, 30.330295],
+    [59.8890801, 30.330295],
+  ], {},
+    {
+      strokeWidth: 5,
+      strokeColor: '#365a49',
+      draggable: false
+    });
+
+
+  console.log(myPolyline);
+
+  myMap.geoObjects.add(myPolyline);
+  myPolyline.editor.startEditing();
+
+  myMap.behaviors.disable('scrollZoom');
+};
+
+ymaps.ready(init);
+
+const mapInfo = document.querySelector('.map-info');
+
+mapInfo.addEventListener('click', function (event) {
+  if (event.target.classList.contains('map-info__trigger')) {
+    event.preventDefault();
+    event.target.closest('.map-info').classList.toggle('map-info--active')
+  }
+});
+
+
